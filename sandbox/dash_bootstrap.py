e45aba229889d465a4fb039162de42ef987b2934
Chengyu <huangchengyu16@gmail.com>
2020-03-01 10:19:46 -0500

    test

--- sandbox/dash_bootstrap.py
+++ sandbox/dash_bootstrap.py
@@ -5,10 +5,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import requests, base64
+from io import BytesIO
 from dash.dependencies import Input, Output, State
 
 
-
+##### dash page build functions ######################3
 def build_filter_check_list(key,options):
     ## hotbutton issues 
     ele = dcc.Checklist(
@@ -18,6 +20,32 @@ def build_filter_check_list(key,options):
                         labelStyle={}
                     )
     return ele
+
+
+
+##############################################################
+    
+
+### build navigation bar ############################
+# make a reuseable navitem for the different examples
+nav_item = dbc.NavItem(dbc.NavLink("SPR Review Automation", href="https://www.imf.org"))
+dropdown = dbc.DropdownMenu(
+    children=[
+        dbc.DropdownMenuItem("Youtube Channel", href='https://www.youtube.com/channel/UC-pBvv8mzLpj0k-RIbc2Nog?view_as=subscriber'),
+        dbc.DropdownMenuItem("Potluck App", href='https://cryptopotluck.com/'),
+        dbc.DropdownMenuItem(divider=True),
+        dbc.DropdownMenuItem("Project Github", href='https://github.com/cryptopotluck/alpha_vantage_tutorial'),
+        dbc.DropdownMenuItem("Plotly / Dash", href='https://dash.plot.ly/'),
+        dbc.DropdownMenuItem("Dash Bootstrap", href='https://dash-bootstrap-components.opensource.faculty.ai/'),
+    ],
+    nav=True,
+    in_navbar=True,
+    label="Important Links",
+)
+
+
+
+#####################################################
 
 filter_tiems_sets = {"content":[{"label":"PRGT",'value':"PRGT"},{"label":"GRA",'value':"GRA"}],
                      "content2":[{"label":"C",'value':"C"},{"label":"D",'value':"D"}]
@@ -62,6 +90,9 @@ cards = dbc.Row(
 
 #%%
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+# try running the app with one of the Bootswatch themes e.g.
+# app = dash.Dash(external_stylesheets=[dbc.themes.JOURNAL])
+# app = dash.Dash(external_stylesheets=[dbc.themes.SKETCHY])
 
 app.layout = html.Div([
     cards,
@@ -73,6 +104,14 @@ app.layout = html.Div([
     ),
     html.H1(id='number-out')
 ])
+
+
+
+############################################################
+
+
+
+
 
 @app.callback(
     Output('number-out', 'children'),
