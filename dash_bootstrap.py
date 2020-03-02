405a7d38d050efbfa1029982164e49c3e370e2c4
Chengyu <huangchengyu16@gmail.com>
2020-03-01 23:23:36 -0500

    very basic version

--- dash_bootstrap.py
+++ dash_bootstrap.py
@@ -1,6 +1,7 @@
 #######
 # A very basic Input/Output callback, with State!
 ######
+import sys
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -8,6 +9,8 @@ import dash_bootstrap_components as dbc
 import requests, base64
 from io import BytesIO
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+sys.path.insert(0,'./dashboard')
 
 
 ##### dash page build functions ######################3
@@ -38,6 +41,35 @@ def build_card(key,options):
         style={'width': "14rem",'height':'100%','border':'0'},
     )
     return onecard
+
+def build_check_items(custom_items_sets):
+    ## hotbutton issues 
+    elements = []
+    for header,check_items in custom_items_sets.items():
+        ele = html.Div(children=[
+                    html.H5(header,
+                            style={'margin': '5px',
+                                   'padding':'5px',
+                                   }),
+                    dcc.Checklist(
+                        id=header,
+                        options=check_items,
+                        value=[],
+                        labelStyle={'display': 'inline-block',
+                                    'padding':"10px",
+                                    'width':'23.5%',
+                                    'borderWidth':'1px',
+                                    'margin':'6px',
+                                    'borderRadius': '5px',
+                                    'borderStyle': 'solid'
+                                    }
+                    )
+                    ],style={'width': '70rem','margin': '10px'}
+                )
+        ele = dbc.Row(ele,justify="center")
+        elements.append(ele)
+    
+    return dbc.Col(id='checklist1',children=elements,style={'display': 'none'})
 ##############################################################
     
 
@@ -100,6 +132,25 @@ filter_tiems_sets = {"Account":[{"label":"PRGT",'value':"PRGT"},{"label":"GRA",'
                      "Disbursement":[{"label":"Disbursing",'value':"Disbursing"},{"label":"Emergency",'value':"Emergency"},
                                      {"label":"Precautionary",'value':"Precautionary"},{"label":"Non-Dispursing",'value':"Non-Dispursing"}],
                       }
+
+Check_tiems_sets = {"Content":[
+                                {"label":"PRGT",'value':"PRGT"},
+                                {"label":"Type of arrangment",'value':"Type of arrangment"},
+                                {"label":"Length of arrangment",'value':"Length of arrangment"},
+                                {"label":"Exceptional access",'value':"Exceptional access"},
+                                ],
+                    "Table":[
+                                {"label":"Selected Economic and Financial Indicator",'value':"Selected Economic and Financial Indicator"},
+                                {"label":"Central/General Government Operations",'value':"Central/General Government Operations"},
+                                {"label":"Balance of payments ",'value':"Balance of payments"},
+                                ],
+                    "Formal Drafting Requirements":[
+                                {"label":"Cover memo",'value':"Cover memo"},
+                                {"label":"Executive summary",'value':"Executive summary"},
+                                {"label":"Program modality",'value':"Program modality"},
+                                ],
+                     }
+
 Fund_color_pallet = [] ## will use color pallet latter
 
 # build cards
@@ -114,9 +165,45 @@ submit_button = dbc.Row(
                            className="mr-1"),
                         justify="center"
                         )   
+submit_button2 = dbc.Row(
+                        dbc.Button(id='submit-button2', 
+                           size="lg",
+                           n_clicks=0,
+                           children='Check', 
+                           className="mr-1"),
+                        justify="center"
+                        )   
+
+file_upload_element = dbc.Row(
+                        dbc.Col(
+                            dcc.Upload(
+                                id='upload-data',
+                                children=html.Div(id='processing_text',children=[
+                                    'Drag and Drop or ',
+                                    html.A('Select Files',style={'color':'blue'})
+                                    ]),
+                                style={
+                                    'width': '60%',
+                                    'height': '80px',
+                                    'lineHeight': '80px',
+                                    'borderWidth': '1px',
+                                    'borderStyle': 'dashed',
+                                    'borderRadius': '5px',
+                                    'textAlign': 'center',
+                                    'background-color':'#cccccc',
+                                    'opacity':'.5',
+                                    'margin': 'auto auto auto auto'
+                                    },
+                                # Allow multiple files to be uploaded
+                                multiple=True
+                                )
+                            ),
+                        justify='center' ,style={'margin':'1rem','display':'none'},id='upload-data-container'
+                        )
 
 #%%
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.config['suppress_callback_exceptions']=True
 # try running the app with one of the Bootswatch themes e.g.
 # app = dash.Dash(external_stylesheets=[dbc.themes.JOURNAL])
 # app = dash.Dash(external_stylesheets=[dbc.themes.SKETCHY])
@@ -125,7 +212,12 @@ app.layout = html.Div([
     navbar,
     cards,
     submit_button,
-    html.Div(id='number-out')
+    file_upload_element,
+    build_check_items(Check_tiems_sets),
+    #html.Div(id='check_items',style={'display': 'block'}),
+    #submit_button2,
+    html.Div(id='intermediate-value',style={'display': 'block'}),
+    html.Div(id='intermediate-value-2',style={'display': 'none'}),
 ])
 
 
@@ -148,18 +240,32 @@ for i in [2]:
     )(toggle_navbar_collapse)
 
 
-checklist_ids = list(filter_tiems_sets.keys())
+filter_ids = list(filter_tiems_sets.keys())
 
 @app.callback(
-    Output('number-out', 'children'),
+    [Output('checklist1', 'style'),Output('upload-data-container', 'style')],
     [Input('submit-button', 'n_clicks')],
-    [State(i,'value') for i in checklist_ids]
+    [State(i,'value') for i in filter_ids]
     )
 def output(n_clicks, i1,i2,i3,i4):
     if sum([len(i1),len(i2),len(i3),len(i4)])==0:
-        return None
+        raise PreventUpdate
     else:
-        return html.P('{} {} {} {}'.format(i1,i2,i3,i4))
+        c_id = "{}-{}-{}-{}".format(i1[0],i2[0],i3[0],i4[0])
+        print(c_id)
+        return [{'display':'block'},{'margin':'1rem','display':'block'}]
 
+
+checklist_ids = list(Check_tiems_sets.keys())
+
+# @app.callback(
+#     Output('intermediate-value', 'children'),
+#     [Input('submit-button2', 'n_clicks')],
+#     [State('Table','value')]
+#     #[State(i,'value') for i in checklist_ids]
+#     )
+# def output(n_clicks, v):
+#     return v
+#%%
 if __name__ == '__main__':
     app.run_server()
